366e6a039826020d7084d7574c452ad460b3a6fe
cmchang1209 <cmchang120956@gmail.com>
2022-02-22 06:00:54 +0000

    update help-handicap-01Game-page

--- resources/js/components/help/handicap/01Game.vue
+++ resources/js/components/help/handicap/01Game.vue
@@ -18,7 +18,7 @@
                             <tbody>
                                 <tr v-for="item in tb1.items" :key="Object.keys(item)[0]">
                                     <td class="text-center pa-0" style="width: 50%;">
-                                        <div class="d-flex justify-center align-center first">{{ Object.keys(item)[0] }}</div>
+                                        <div class="d-flex justify-center align-center">{{ Object.keys(item)[0] }}</div>
                                     </td>
                                     <td class="text-center pa-0">
                                         <div class="d-flex justify-center align-center">{{ Object.values(item)[0] }}</div>
